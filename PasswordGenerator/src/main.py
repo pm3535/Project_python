9aa9310678adbf177dbadd13c53239f9212c6648
pardismir <pardis.m1989@gmail.com>
2026-05-12 20:20:31 +0330

    complete memorable password

--- PasswordGenerator/src/main.py
+++ PasswordGenerator/src/main.py
@@ -1,7 +1,11 @@
 import random
 import string
-from typing import List
+from typing import List, Optional
 from abc import ABC, abstractmethod
+
+import nltk
+
+nltk.download('words')
 
 class PasswordGenerator(ABC):
     @abstractmethod
@@ -20,3 +24,31 @@ class PasswordGenerator(ABC):
 
         def generate(self) -> str:
             return ''.join(random.choice(self.characters) for _ in range(self.length))
+        
+    class MemorablePasswordGenerator(PasswordGenerator):
+
+      def __init__(
+        self,
+        no_of_words: int = 5,
+        separator: str = "-",
+        capitalization: bool = False,
+        vocabulary: Optional[List[str]] = None
+    ):
+        if vocabulary is None:
+            vocabulary = nltk.corpus.words.words()  # edit this to any vocabulary list you want
+
+        self.no_of_words: int = no_of_words
+        self.separator: str = separator
+        self.capitalization: bool = capitalization
+        self.vocabulary: List[str] = vocabulary
+
+    def generate(self) -> str:
+
+        password_words = [random.choice(self.vocabulary) for _ in range(self.no_of_words)]
+        if self.capitalization:
+            password_words = [word.upper() for word in password_words]
+        return self.separator.join(password_words)
+
+
+        
+
