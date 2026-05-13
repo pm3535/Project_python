0d5149bfdc38d63d3cf77ad6ddd6e90b28fc2757
pardismir <pardis.m1989@gmail.com>
2026-05-13 13:08:35 +0330

    completed streamli

--- Streamlit Dashboard/src/app.py
+++ Streamlit Dashboard/src/app.py
@@ -1,2 +1,33 @@
 import streamlit as st
 from password_generator import RandomPasswordGenerator, MemorablePasswordGenerator, PinCodeGenerator
+from nltk.corpus import words
+
+st.image('./images/banner.jpeg')
+st.title(":zap: Password Generator")
+
+option = st.radio("Password Type", ('Random Password', 'Memorable Password', 'Pin Code'))
+
+if option == 'Random Password':
+    length = st.slider("Password Length", min_value=5, max_value=50, value=8)
+    include_numbers = st.toggle("Include Numbers")
+    include_symbols = st.toggle("Include Symbols")
+
+    generator = RandomPasswordGenerator(length=length, include_numbers=include_numbers, include_symbols=include_symbols)
+elif option == 'Memorable Password':
+    no_of_words = st.slider("Number of Words", min_value=2, max_value=10, value=5)
+    separator = st.text_input("Separator", value="-")
+    capitalization = st.toggle("Capitalize Words")
+
+    generator = MemorablePasswordGenerator(no_of_words, separator, capitalization, words.words())
+
+else:
+    length = st.slider("Pin Code Length", min_value=2, max_value=8, value=4)
+    generator = PinCodeGenerator(length)
+
+    password = generator.generate()
+    st.write("Your password is:")
+    st.header(fr"``` {password} ```")
+
+
+
+
